98d77bb276b5e28e5048c2f5eb5c34034f9545ef
BitHachi <bithachi@163.com>
2020-06-14 23:35:47 +0800

    1.1.3

---
+++ src/Chapter1/Section11/EX1_1_3.java
@@ -0,0 +1,21 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX03
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:11
+ * @Version: V1.0
+ * @Description: 练习题1.1.3
+ **/
+
+public class EX1_1_3 {
+    public static void main(String[] args) {
+        /************  1.1.3  ************/
+        if(args[0].equals(args[1])&&args[1].equals(args[2]))
+            System.out.println("equal");
+        else  System.out.println("not equal");
+
+    }
+}
+

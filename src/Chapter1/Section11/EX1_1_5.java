9beec3f561fcd2170f058067c9fbe6e5e8b857c1
BitHachi <bithachi@163.com>
2020-06-14 23:36:08 +0800

    1.1.5

---
+++ src/Chapter1/Section11/EX1_1_5.java
@@ -0,0 +1,22 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX04
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:11
+ * @Version: V1.0
+ * @Description: 练习题1.1.4
+ **/
+
+public class EX1_1_5 {
+    public static void main(String[] args) {
+        /************  1.1.5  ************/
+        double x=0.8,y=1.9;
+        if(x>=0&&y>=0&&x<=1&&y<=1)
+            System.out.println(true);
+        else System.out.println(false);
+
+    }
+}
+

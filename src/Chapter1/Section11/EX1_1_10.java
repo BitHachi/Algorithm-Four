7c384cc92e8c2c1129af333d9f62e5b9d0fa26cd
BitHachi <bithachi@163.com>
2020-06-14 23:37:28 +0800

    1.1.10

---
+++ src/Chapter1/Section11/EX1_1_10.java
@@ -0,0 +1,22 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX1_1_10
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:18
+ * @Version: V1.0
+ * @Description: 练习题1.1.10
+ **/
+
+public class EX1_1_10 {
+    public static void main(String[] args) {
+        /************  1.1.10  ************/
+        //会报没有初始化数组的错误
+//        int[] a;
+//        for (int i = 0; i <10 ; i++) {
+//            a[i]=i*i;
+//        }
+    }
+}
+

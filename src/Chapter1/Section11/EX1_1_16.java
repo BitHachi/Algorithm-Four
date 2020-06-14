670d3a17c94d9b1e38142f8d9a1901f7239bb69e
BitHachi <bithachi@163.com>
2020-06-14 23:39:11 +0800

    1.1.16

---
+++ src/Chapter1/Section11/EX1_1_16.java
@@ -0,0 +1,22 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX1_1_16
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:27
+ * @Version: V1.0
+ * @Description: 练习题1.1.16
+ **/
+
+public class EX1_1_16 {
+    public static void main(String[] args) {
+        /************  1.1.16  ************/
+        System.out.println(exR1(6));
+    }
+    public static String exR1(int n) {
+        if (n <= 0) return "";
+        return exR1(n - 3) + n + exR1(n - 2) + n;
+    }
+}
+

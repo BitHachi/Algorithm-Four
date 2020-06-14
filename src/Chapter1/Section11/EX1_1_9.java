3bfcb1d3505b442da8563a2aba7162d929613b4e
BitHachi <bithachi@163.com>
2020-06-14 23:37:13 +0800

    1.1.9

---
+++ src/Chapter1/Section11/EX1_1_9.java
@@ -0,0 +1,24 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX1_1_9
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:17
+ * @Version: V1.0
+ * @Description: 练习题1.1.9
+ **/
+
+public class EX1_1_9 {
+    public static void main(String[] args) {
+        /************  1.1.9  ************/
+        int N=100;
+        System.out.println(Integer.toBinaryString(N));
+        String s="";
+        //从低位往高位求
+        for(int n=N;n>0;n/=2)
+            s=(n%2)+s;
+        System.out.println(s);
+    }
+}
+

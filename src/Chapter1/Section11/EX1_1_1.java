d491a71bd7f5e69bd0d64a1671b4b7d784decbc9
BitHachi <bithachi@163.com>
2020-06-14 23:34:45 +0800

    1.1.1

---
+++ src/Chapter1/Section11/EX1_1_1.java
@@ -0,0 +1,21 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX01
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 19:23
+ * @Version: V1.0
+ * @Description: 练习题1.1.1
+ **/
+
+public class EX1_1_1 {
+    public static void main(String[] args) {
+        /************  1.1.1  ************/
+        System.out.println((0+15)/2);
+        System.out.println(2.0e-6*100000000.1);
+        System.out.println(true&&false||true&&true);
+
+    }
+}
+

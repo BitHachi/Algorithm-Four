02e39dcd0b1f5759b434c161ae7d5060d0487674
BitHachi <bithachi@163.com>
2020-06-14 23:36:57 +0800

    1.1.8

---
+++ src/Chapter1/Section11/EX1_1_8.java
@@ -0,0 +1,21 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX1_1_8
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-14 23:16
+ * @Version: V1.0
+ * @Description: 练习题1.1.8
+ **/
+
+public class EX1_1_8 {
+    public static void main(String[] args) {
+        /************  1.1.8  ************/
+        System.out.println('b');
+        System.out.println('b'+'c');//98+99
+        System.out.println('a'+4);//97+4
+        System.out.println((char)('a'+4));//97+4
+    }
+}
+

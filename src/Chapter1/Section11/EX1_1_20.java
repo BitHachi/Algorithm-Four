fcfada290af9c66273cc0cdd73b82c30a00c2713
BitHachi <312353973@qq.com>
2020-06-20 18:27:19 +0800

    1.1.20

---
+++ src/Chapter1/Section11/EX1_1_20.java
@@ -0,0 +1,27 @@
+package Chapter1.Section11;
+
+/**
+ * @Program: Algorithm
+ * @ClassName: EX1_1_20
+ * @Author: Mr.BitHachi
+ * @CreateTime: 2020-06-20 17:18
+ * @Version: V1.0
+ * @Description: 练习题1.1.20
+ **/
+
+public class EX1_1_20 {
+    public static double ln(int N) {
+        if (N == 0) {
+            return 0;
+        } else {
+            /*static double log(double a)
+            * 返回的自然对数（以 e为底） double值。 */
+            return Math.log(N) + ln(N - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ln(10));
+    }
+}
+
